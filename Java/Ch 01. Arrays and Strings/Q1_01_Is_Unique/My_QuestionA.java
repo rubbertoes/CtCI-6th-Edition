bc8388cd84c2f53a2279e2ff32f7ee299d42c056
Robert Schattner <robertvschattner@gmail.com>
2024-03-26 15:31:58 -0400

    Checking connection

---
+++ Java/Ch 01. Arrays and Strings/Q1_01_Is_Unique/My_QuestionA.java
@@ -0,0 +1,15 @@
+package Q1_01_Is_Unique;
+
+public class My_QuestionA {
+   	public static boolean isUniqueChars(String str) {
+        return false;
+	}
+	
+	public static void main(String[] args) {
+		String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+		for (String word : words) {
+			System.out.println(word + ": " + isUniqueChars(word));
+		}
+	}
+ 
+}
